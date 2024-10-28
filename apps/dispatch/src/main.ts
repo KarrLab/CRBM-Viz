0fa08ceb3e5fc127b7c60e4143ecdb485db24261
spaceBearAmadeus (Alex) <alexanderpatrie@gmail.com>
2024-10-28 12:12:47 -0400

    chore: added marking comments for dispatch redirect clientside

--- apps/dispatch/src/main.ts
+++ apps/dispatch/src/main.ts
@@ -12,6 +12,7 @@ platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .catch((err) => console.error(err));
 
+// Redirect dispatch to platform. TODO: do this in netlify, but okay for now. Okay to merge.
 if (window.location.hostname === 'run.biosimulations.org') {
   window.location.href = 'https://biosimulations.org';
 }
